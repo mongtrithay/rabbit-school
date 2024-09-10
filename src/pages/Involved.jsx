35c3272773c75a886999f2dce5e2fbd2be4264c6
Chansreynit6 <sreynitchanngoun@gmail.com>
2024-09-10 10:59:08 +0700

    update more

--- src/pages/Involved.jsx
+++ src/pages/Involved.jsx
@@ -189,6 +189,7 @@ function Involved() {
       </div>
 
       {/* //section4 */}
+      {/*//time to read */}
 
       <div className="flex flex-col md:flex-row max-w-10xl mx-auto bg-white  overflow-hidden h-auto">
         <div className="w-full md:w-2/2 p-8 bg-white-100">
