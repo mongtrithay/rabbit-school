1708bc9124e17d4382344480bfed471353618763
mongtrithay <trithay.mong@institute.pse.ngo>
2024-09-10 10:41:11 +0700

    update more

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import ButtonDonate from './companents/ButtonDonate'
 import ButtonReadMore from './companents/ButtonReadMore'
 import ButtonTranslate from './companents/ButtonTranslate'
 import Footer from './companents/Footer'
-<<<<<<< HEAD
 import KeyFigures from './companents/KeyFigures'
-=======
 import SliderAction from './companents/SliderAction'
->>>>>>> main
 
 
 
