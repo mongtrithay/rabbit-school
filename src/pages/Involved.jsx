5a7b1d8de02e4bc39d3006245d69e47714daefcc
Chansreynit6 <sreynitchanngoun@gmail.com>
2024-09-10 10:47:22 +0700

    finished all

--- src/pages/Involved.jsx
+++ src/pages/Involved.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Menu from "../companents/Menu";
 import Footer from "../companents/Footer";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBookOpenReader,
@@ -316,7 +317,6 @@ function Involved() {
               className="w-[400px] p-3 text-[20px] border border-sky-700  focus:outline-none focus:ring-2 focus:ring-sky-500"
             />
 
-            
             <input
               type="email"
               placeholder="Email"
