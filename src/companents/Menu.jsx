e86dd7b15f0c24169a4ad58c517ae5047819dd4a
seavmeyngat <seavmey.ngat@institute.pse.ngo>
2024-09-11 16:29:57 +0700

    update

--- src/companents/Menu.jsx
+++ src/companents/Menu.jsx
@@ -3,40 +3,38 @@ import imageSrc from '../assets/images/img1.png';
 
 function Menu() {
     return (
-        <div className="flex items-center p-2 w-full h-200 bg-sky-700">
-            <div className="flex-shrink-0 mx-[50px]">
-                <img className="w-20 h-20 object-contain" src={imageSrc} alt="Logo" />
+        <header className="flex items-center justify-between w-full h-20 bg-sky-700 p-4">
+            <div className="flex items-center">
+                <img className="w-16 h-16 object-contain" src={imageSrc} alt="Logo" />
             </div>
-            <div className='flex items-center justify-end w-full h-200 '>
-                <div className="mr-[50px] ">
-                    <ul className="flex gap-20 text-white font-semibold ">
-                        <li>
-                            <a href="#" className="hover:text-gray-600 transition-colors text-[20px]">HOME</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:text-gray-600 transition-colors text-[20px]">ABOUT US</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:text-gray-600 transition-colors text-[20px]">OUR ACTIONS</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:text-gray-600 transition-colors text-[20px]">GET INVOLVED</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:text-gray-600 transition-colors text-[20px]">SPORT US</a>
-                        </li>
-                        <li className="">
-                            <a href="#" className="hover:text-gray-600 transition-colors text-[20px]">CONTACT</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="flex items-center gap-4 mr-[50px] ">
-                    <a href="#" className="text-white text-xl font-semibold hover:text-gray-600 transition-colors">ENG</a>
+            <nav className="flex items-center w-full justify-end">
+                <ul className="flex gap-12 text-white font-semibold mr-24">
+                    <li>
+                        <a href="#" className="hover:text-gray-400 transition-colors text-lg">HOME</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-gray-400 transition-colors text-lg">ABOUT US</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-gray-400 transition-colors text-lg">OUR ACTIONS</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-gray-400 transition-colors text-lg">GET INVOLVED</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-gray-400 transition-colors text-lg">SUPPORT US</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-gray-400 transition-colors text-lg">CONTACT</a>
+                    </li>
+                </ul>
+                <div className="flex items-center gap-4 ml-8">
+                    <a href="#" className="text-white text-lg font-semibold hover:text-gray-400 transition-colors">ENG</a>
                     <div className="h-5 border-l border-gray-400"></div>
-                    <a href="#" className="text-white text-xl font-semibold hover:text-gray-600 transition-colors">KH</a>
+                    <a href="#" className="text-white text-lg font-semibold hover:text-gray-400 transition-colors">KH</a>
                 </div>
-            </div>
-        </div>
+            </nav>
+        </header>
     );
 }
 
