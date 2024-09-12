1d947a894685808cf368d79f2c8087a0d9054889
seavmeyngat <seavmey.ngat@institute.pse.ngo>
2024-09-12 15:53:16 +0700

    support page

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,6 +1,7 @@
 import Menu from "../companents/Menu";
-import image14 from "../assets/images/image14.png";
+import image15 from "../assets/images/image15.png"; // Corrected image import
 import Footer from "../companents/Footer";
+
 function SupportPage(){
     return(
         <>
@@ -34,7 +35,7 @@ function SupportPage(){
                         <p><span className="font-bold"> Account number:</span>  2900-01-005152-1-2</p>
                     </div>
                     <div className="flex-1">
-                        <img src={image15} alt="" className="w-full sm:w-[340px] h-auto "/>
+                        <img src={image15} alt="Donation" className="w-full sm:w-[340px] h-auto "/> {/* Updated image source */}
                     </div>
                 </div>
             </div>
@@ -44,38 +45,35 @@ function SupportPage(){
                 We thank you for your generosity.
             </p>
 
-            {/* <div className="mt-16">
+            <div className="flex flex-col sm:flex-row w-[90%] sm:w-[65%] m-auto gap-6 sm:gap-10 mt-10">
+                <div className="flex-1 text-center">
+                    <h1 className="text-[25px] sm:text-[30px] text-sky-700 font-bold">Address</h1>
+                    <p className="text-[18px] sm:text-[20px] text-gray-500">
+                        https:/goo.glmapsjvMBC2MgTz1kUyoh6
+                    </p>
+                </div>
+                <div className="flex-1 text-center">
+                    <h1 className="text-[25px] sm:text-[30px] text-sky-700 font-bold">Email</h1>
+                    <p className="text-[18px] sm:text-[20px] text-gray-500">
+                        sor.sothearom@rabbitschoolcambodia.net <br />
+                        hun.touch@rabbitschoolcambodia.net
+                    </p>
+                </div>
+                <div className="flex-1 text-center">
+                    <h1 className="text-[25px] sm:text-[30px] text-sky-700 font-bold">Phone </h1>
+                    <p className="text-[18px] sm:text-[20px] text-gray-500">
+                        +855 68 901 971 <br />
+                        +855 17 525 815
+                    </p>
+                </div>
+            </div>
+            
+            {/* <div className="mt-20">
                 <Footer />
             </div> */}
         </div>
-
-        <div className="flex flex-col sm:flex-row w-[90%] sm:w-[65%] m-auto gap-6 sm:gap-10 mt-10">
-          <div className="flex-1 text-center">
-            <h1 className="text-[25px] sm:text-[30px] text-sky-700 font-bold">Address</h1>
-            <p className="text-[18px] sm:text-[20px] text-gray-500">
-              https:/goo.glmapsjvMBC2MgTz1kUyoh6
-            </p>
-          </div>
-          <div className="flex-1 text-center">
-            <h1 className="text-[25px] sm:text-[30px] text-sky-700 font-bold">Email</h1>
-            <p className="text-[18px] sm:text-[20px] text-gray-500">
-              sor.sothearom@rabbitschoolcambodia.net <br />
-              hun.touch@rabbitschoolcambodia.net
-            </p>
-          </div>
-          <div className="flex-1 text-center">
-            <h1 className="text-[25px] sm:text-[30px] text-sky-700 font-bold">Phone </h1>
-            <p className="text-[18px] sm:text-[20px] text-gray-500">
-              +855 68 901 971 <br />
-              +855 17 525 815
-            </p>
-          </div>
-        </div>
-        <div className="mt-20">
-          <Footer />
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
+
 export default SupportPage;
