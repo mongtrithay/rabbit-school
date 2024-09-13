014401df8f96ade6009a9c301d837528d2f1861f
mongtrithay <trithay.mong@institute.pse.ngo>
2024-09-13 09:41:28 +0700

    router link

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AboutPage from "./pages/About.jsx";
+import ContactPage from "./pages/Contact.jsx";
+import Homepage from "./pages/Home.jsx";
+import Involved from "./pages/Involved.jsx";
+import OurAction from "./pages/OurAction.jsx";
+import SupportPage from "./pages/Support.jsx";
+import Detail from "./pages/Detail.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/aboutpage",
+    element: <AboutPage />,
+  },
+  {
+    path: '/contactpage',
+    element: <ContactPage />
+  },
+  {
+    path: '/involved',
+    element: <Involved />
+  },
+  {
+    path: '/ouraction',
+    element: <OurAction />
+  },
+  {
+    path: '/supportpage',
+    element: <SupportPage />
+  },
+  {
+    path: '/detail',
+    element: <Detail />
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
