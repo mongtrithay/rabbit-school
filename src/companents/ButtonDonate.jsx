6e20d898ba7454bc1c73b43f34710ccf44ad1f38
mongtrithay <trithay.mong@institute.pse.ngo>
2024-09-26 07:09:14 +0700

    update button donate

--- src/companents/ButtonDonate.jsx
+++ src/companents/ButtonDonate.jsx
@@ -3,7 +3,7 @@ function ButtonDonate() {
   return (
 
     <button className="lg:m-2 lg:px-4 px-2 lg:py-2 py-1 text-white font-bold bg-orange-500 lg:text-[25px] text-[15px] mt-5">
-        D0NATE
+        DONATE
     </button>
   );
 }
